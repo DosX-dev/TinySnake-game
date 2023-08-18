eb26c89ccc63f49c6f1538edf08db3e4367f273f
DosX <117774904+DosX-dev@users.noreply.github.com>
2023-08-18 21:01:00 +0300

    Update snake.c

--- source/snake.c
+++ source/snake.c
@@ -1,10 +1,12 @@
 // Coded by DosX
 
 #include <conio.h>
-#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <windows.h>
+
+#include "colors.h"
 
 #define SIZE 25
 
@@ -13,49 +15,16 @@
 
 #define SPEED_DELAY 100
 
-// Define foreground text colors
-#define FOREGROUND_BLACK 0x00
-#define FOREGROUND_BLUE 0x01
-#define FOREGROUND_GREEN 0x02
-#define FOREGROUND_RED 0x04
-#define FOREGROUND_GRAY 0x08
-#define FOREGROUND_CYAN (FOREGROUND_GREEN | FOREGROUND_BLUE)
-#define FOREGROUND_MAGENTA (FOREGROUND_RED | FOREGROUND_BLUE)
-#define FOREGROUND_YELLOW (FOREGROUND_RED | FOREGROUND_GREEN)
-#define FOREGROUND_WHITE (FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE)
-#define FOREGROUND_LIGHT_BLUE (FOREGROUND_BLUE | FOREGROUND_INTENSITY)
-#define FOREGROUND_LIGHT_GREEN (FOREGROUND_GREEN | FOREGROUND_INTENSITY)
-#define FOREGROUND_LIGHT_CYAN (FOREGROUND_CYAN | FOREGROUND_INTENSITY)
-#define FOREGROUND_LIGHT_RED (FOREGROUND_RED | FOREGROUND_INTENSITY)
-#define FOREGROUND_LIGHT_MAGENTA (FOREGROUND_MAGENTA | FOREGROUND_INTENSITY)
-#define FOREGROUND_LIGHT_YELLOW (FOREGROUND_YELLOW | FOREGROUND_INTENSITY)
-#define FOREGROUND_BRIGHT_WHITE (FOREGROUND_WHITE | FOREGROUND_INTENSITY)
-
-// Define background colors
-#define BACKGROUND_BLACK 0x00
-#define BACKGROUND_BLUE 0x10
-#define BACKGROUND_GREEN 0x20
-#define BACKGROUND_RED 0x40
-#define BACKGROUND_GRAY 0x80
-#define BACKGROUND_CYAN (BACKGROUND_GREEN | BACKGROUND_BLUE)
-#define BACKGROUND_MAGENTA (BACKGROUND_RED | BACKGROUND_BLUE)
-#define BACKGROUND_YELLOW (BACKGROUND_RED | BACKGROUND_GREEN)
-#define BACKGROUND_WHITE (BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE)
-#define BACKGROUND_LIGHT_BLUE (BACKGROUND_BLUE | BACKGROUND_INTENSITY)
-#define BACKGROUND_LIGHT_GREEN (BACKGROUND_GREEN | BACKGROUND_INTENSITY)
-#define BACKGROUND_LIGHT_CYAN (BACKGROUND_CYAN | BACKGROUND_INTENSITY)
-#define BACKGROUND_LIGHT_RED (BACKGROUND_RED | BACKGROUND_INTENSITY)
-#define BACKGROUND_LIGHT_MAGENTA (BACKGROUND_MAGENTA | BACKGROUND_INTENSITY)
-#define BACKGROUND_LIGHT_YELLOW (BACKGROUND_YELLOW | BACKGROUND_INTENSITY)
-#define BACKGROUND_BRIGHT_WHITE (BACKGROUND_WHITE | BACKGROUND_INTENSITY)
-
 #define true 0b1
 #define false 0b0
 #define short 32768
 #define null (void *)0b0
 
-int gameover = false, score = 0, snakeX[short], snakeY[short], snake_length = 1,
-    foodX = 0, foodY = 0, dirX = 0, dirY = 0;
+int gameover = false,
+    score = 0, snakeX[short], snakeY[short],
+    snake_length = 1,
+    foodX = 0, foodY = 0,
+    dirX = 0, dirY = 0;
 
 HANDLE hConsole = null;
 CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
@@ -64,8 +33,7 @@ void setup() {
     snakeX[0] = WIDTH / 2;
     snakeY[0] = HEIGHT / 2;  // Set snake head at center of game zone
 
-    srand(time(
-        null));  // Initialize the random number generator with the current time
+    srand(time(0x00));  // Initialize the random number generator with the current time
     foodX = 1 + rand() % (WIDTH - 2);
     foodY = 1 + rand() % (HEIGHT - 2);
 
@@ -77,7 +45,7 @@ void refillRenderZone() {
     SetConsoleCursorPosition(hConsole, (COORD){0, 2});  // Skip 2 lines
 }
 
-void draw(text, color) {
+void draw(char *text, int color) {
     GetConsoleScreenBufferInfo(hConsole, &consoleInfo);
     SetConsoleTextAttribute(hConsole, color);
     printf(text);
@@ -88,18 +56,16 @@ void draw(text, color) {
 void render() {
     refillRenderZone();
 
-    int i, j;
-
-    for (i = 0; i < HEIGHT; i++)  // Loop through the game board rows
+    for (int i = 0; i < HEIGHT; i++)  // Loop through the game board rows
     {
-        for (j = 0; j < WIDTH; j++)  // Loop through the game board columns
+        for (int j = 0; j < WIDTH; j++)  // Loop through the game board columns
         {
-            if (i == 0 || i == HEIGHT - 1 || j == 0 || j == WIDTH - 1)  // Check if it's a border cell
-                draw("  ", BACKGROUND_CYAN);                            // Draw a cyan-colored border
-            else if (i == snakeY[0] && j == snakeX[0])                  // Check if it's the snake's head cell
-                draw("^^", BACKGROUND_WHITE | FOREGROUND_BLACK);        // Draw the snake's head with eyes
-            else if (i == foodY && j == foodX)                          // Check if it's the food cell
-                draw("  ", BACKGROUND_LIGHT_RED);                       // Draw an apple (food)
+            if (i == 0 || i == HEIGHT - 1 || j == 0 || j == WIDTH - 1)               // Check if it's a border cell
+                draw("  ", !gameover ? BACKGROUND_CYAN : BACKGROUND_RED);            // Draw a cyan-colored border
+            else if (i == snakeY[0] && j == snakeX[0])                               // Check if it's the snake's head cell
+                draw(!gameover ? "^^" : "oO", BACKGROUND_WHITE | FOREGROUND_BLACK);  // Draw the snake's head with eyes
+            else if (i == foodY && j == foodX)                                       // Check if it's the food cell
+                draw("  ", BACKGROUND_LIGHT_RED);                                    // Draw an apple (food)
             else {
                 int isBody = false;
                 for (int k = 1; k < snake_length; k++)  // Loop through the snake's body cells
@@ -166,7 +132,9 @@ void input() {
 
 // Function to update the game logic
 void logic() {
-    int prevX = snakeX[0], prevY = snakeY[0], prev2X, prev2Y;
+    int prevX = snakeX[0],
+        prevY = snakeY[0],
+        prev2X, prev2Y;
     snakeX[0] += dirX;
     snakeY[0] += dirY;
 
@@ -191,8 +159,7 @@ void logic() {
 
     // Check if the snake eats the food
     if (snakeX[0] == foodX && snakeY[0] == foodY) {
-        // Generate new random coordinates for food within the game field
-        // (excluding the borders)
+        // Generate new random coordinates for food within the game field (excluding the borders)
         do {
             foodX = 1 + rand() % (WIDTH - 2);
             foodY = 1 + rand() % (HEIGHT - 2);
@@ -207,16 +174,14 @@ void logic() {
             }
         }
 
-        // If the food spawns on the snake's body, try generating new
-        // coordinates again
+        // If the food spawns on the snake's body, try generating new coordinates again
         while (spawnOnBody) {
             foodX = 1 + rand() % (WIDTH - 2);
             foodY = 1 + rand() % (HEIGHT - 2);
 
             spawnOnBody = false;
             for (int i = 0; i < snake_length; i++) {
-                // If the coordinates of the Apple are on the body of the snake,
-                // then the cycle continues
+                // If the coordinates of the Apple are on the body of the snake, then the cycle continues
                 if (foodX == snakeX[i] && foodY == snakeY[i]) {
                     spawnOnBody = true;
                     break;
@@ -249,14 +214,8 @@ void logic() {
 int getBitness() { return sizeof(void *) == 0x4 ? 32 : 64; }
 
 int main() {
-    char title[short] = "Tiny Snake (x";
-
-    // Преобразуем числовое значение указателя в строку с помощью sprintf
-    char sizeStr[5];
-    sprintf(sizeStr, "%d", getBitness());
-
-    strcat(title, sizeStr);
-    strcat(title, ")");
+    char title[short];
+    sprintf(title, "Tiny Snake (x%d)", getBitness());
 
     SetConsoleTitleA(title);
 
@@ -268,11 +227,7 @@ int main() {
     SMALL_RECT windowSize = {0, 0, buffer[0] - 1, buffer[1] - 1};
     SetConsoleWindowInfo(hConsole, true, &windowSize);
 
-    setlocale(LC_ALL, ".65001");
-
-    printf(
-        "Coded by DosX-dev (GitHub)\nUSE ONLY ENGLISH KEYBOARD LAYOUT! "
-        "(WASD)\n");
+    printf("Coded by DosX-dev (GitHub)\nUSE ONLY ENGLISH KEYBOARD LAYOUT! (WASD)\n");
 
     setup();  // Initialize the game
 
@@ -283,6 +238,11 @@ int main() {
 
         Sleep(SPEED_DELAY);  // Add a delay to control the snake's speed
     }
+
+    dirX = 0;
+    dirY = 0;
+
+    render();  // Run the renderer for the last time to draw the desired colors (lose)
 
     printf("\n");
 
